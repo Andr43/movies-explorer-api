8989d67f0adf364b2764d96a08f06d7d77938147
Andrey Musinov <andrew.musinov@gmail.com>
2023-05-30 23:19:31 +0100

    Добавил схемы и модели для users и movies

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/mestodb', {
+mongoose.connect('mongodb://127.0.0.1/projectmoviesdb', {
   useNewUrlParser: true,
 });
 app.use(express.json());
---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+const { isEmail } = require('validator');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator(v) {
+          return isEmail(v);
+        },
+        message: (props) => `${props.value} не соответствует формату email!`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { toJSON: { useProjection: true }, toObject: { useProjection: true }, versionKey: false },
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Необходима авторизация.'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Необходима авторизация.'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
