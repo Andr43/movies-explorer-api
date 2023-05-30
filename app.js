1e7528ae07c9c8aee670a0b5e1d2e4d35dfc5629
Andrey Musinov <andrew.musinov@gmail.com>
2023-05-30 19:35:04 +0100

    Сделал всю подготовительную часть проекта

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1/mestodb', {
+  useNewUrlParser: true,
+});
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
