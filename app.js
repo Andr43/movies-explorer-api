786570380f9cdbea73ea03c2916510fcf8f806cc
Andrey Musinov <andrew.musinov@gmail.com>
2023-06-02 20:36:46 +0100

    Добавил логгеры для запросов и ошибок

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const router = require('./routes/index');
 const cookieParser = require('cookie-parser');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://127.0.0.1/projectmoviesdb', {
   useNewUrlParser: true,
 });
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
