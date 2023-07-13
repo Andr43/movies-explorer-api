79541d430679f9b5387e1c298cf28f3c27b62087
Andrey Musinov <andrew.musinov@gmail.com>
2023-07-13 13:11:02 +0100

    Добавил заголовок secure

--- controllers/users.js
+++ controllers/users.js
@@ -74,6 +74,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'None',
+          secure: true,
         })
         .send(token)
         .end();
