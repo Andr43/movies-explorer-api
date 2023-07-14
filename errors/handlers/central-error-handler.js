48c4c3d85c9e598720c28af80dfc21e00a07634d
Andrey Musinov <andrew.musinov@gmail.com>
2023-07-14 16:32:09 +0100

    Добавил в обработчик ошибок передачу клиенту ошибки в формате JSON

--- errors/handlers/central-error-handler.js
+++ errors/handlers/central-error-handler.js
@@ -6,7 +6,7 @@ const centralErrorHandler = (err, req, res, next) => {
   const { statusCode = HTTP_STATUS_INTERNAL_SERVER_ERROR, message } = err;
   res
     .status(statusCode)
-    .send({
+    .json({
       message: statusCode === HTTP_STATUS_INTERNAL_SERVER_ERROR
         ? 'На сервере произошла ошибка.'
         : message,
